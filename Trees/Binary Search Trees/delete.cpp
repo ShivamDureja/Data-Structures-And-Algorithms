4e78df263ea9831c3f75d8730600cfa009717718
Shivam Dureja <shivamdureja13@gmail.com>
2021-11-24 20:29:41 +0530

    delete.cpp added

--- Trees/Binary Search Trees/delete.cpp
+++ Trees/Binary Search Trees/delete.cpp
@@ -82,9 +82,77 @@ struct Node *RInsert(struct Node *p, int key)
     return p;
 }
 
-int main()
+int Height(struct Node *p)
 {
 
+    int x, y;
+    if (p == NULL)
+        return 0;
+    x = Height(p->lchild);
+    y = Height(p->rchild);
+    return x > y ? x + 1 : y + 1;
+}
+
+struct Node *InPre(struct Node *p)
+{
+    while (p && p->rchild != NULL)
+    {
+        p = p->rchild;
+    }
+    return p;
+}
+struct Node *InSucc(struct Node *p)
+{
+    while (p && p->lchild != NULL)
+    {
+        p = p->lchild;
+    }
+    return p;
+}
+
+struct Node *Delete(struct Node *p, int key)
+{ //Deleting in BST (important)
+
+    if (p == NULL)
+        return NULL;
+    if (p->lchild == NULL && p->rchild == NULL)
+    {
+        if (p == root)
+            root = NULL;
+        free(p);
+        return NULL;
+    }
+
+    struct Node *q;
+    if (key < p->data)
+    {
+        p->lchild = Delete(p->lchild, key);
+    }
+    else if (key > p->data)
+    {
+        p->rchild = Delete(p->rchild, key);
+    }
+    else
+    {
+        if (Height(p->lchild) > Height(p->rchild))
+        {
+            q = InPre(p->lchild);
+            p->data = q->data;
+            p->lchild = Delete(p->lchild, q->data);
+        }
+        else
+        {
+            q = InSucc(p->rchild);
+            p->data = q->data;
+            p->rchild = Delete(p->rchild, q->data);
+        }
+    }
+    return p;
+}
+
+int main()
+{
+    int y;
     root = RInsert(root, 50);
     RInsert(root, 15);
     RInsert(root, 5);
@@ -92,6 +160,11 @@ int main()
     RInsert(root, 8);
     RInsert(root, 30);
     cout << "Final BST is ->" << endl;
+    InOrder(root);
+    cout<<"Enter the element u want to delete"<<endl;
+    cin>>y;
+    Delete(root,y);
+    cout<<"Tree after deletion is:"<<endl;
     InOrder(root);
 
     return 0;
