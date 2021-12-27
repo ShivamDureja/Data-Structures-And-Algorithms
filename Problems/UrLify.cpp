11fe4037027f5c4d360287fefa0875596f9771de
Shivam Dureja <shivamdureja13@gmail.com>
2021-12-27 22:48:37 +0530

    Create UrLify.cpp

---
+++ Problems/UrLify.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstring>
+
+void urlify(char *str, int len)
+{
+    int numOfSpaces = 0;
+    int i = 0, j = 0;
+    for (i = 0; i < len; ++i)
+    {
+        if (str[i] == ' ')
+        {
+            ++numOfSpaces;
+        }
+    }
+
+    int extendedLen = len + 2 * numOfSpaces;
+    i = extendedLen - 1;
+    for (j = len - 1; j >= 0; --j)
+    {
+        if (str[j] != ' ')
+        {
+            str[i--] = str[j];
+        }
+        else
+        {
+            str[i--] = '0';
+            str[i--] = '2';
+            str[i--] = '%';
+        }
+    }
+}
+
+int main()
+{
+    char str[] = "Mr John Smith         "; //String with extended length ( true length + 2* spaces)
+    std::cout << "Actual string   : " << str << std::endl;
+    urlify(str, 13); //Length of "Mr John Smith" = 13
+    std::cout << "URLified string : " << str << std::endl;
+    return 0;
+}
