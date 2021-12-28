bdcb0c34ed7ac362538172ead69b153b4f81e04b
Shivam Dureja <shivamdureja13@gmail.com>
2021-12-28 19:54:06 +0530

    Update stringrotation.cpp

--- Problems/stringrotation.cpp
+++ Problems/stringrotation.cpp
@@ -5,7 +5,7 @@ bool isRotation(string s1, string s2)
 {
 
     string temp = s1 + s1;
-    return (temp.find(s2) != string::npos);
+    return (temp.find(s2) != string::npos);  //string::npos used to indicate end of string
 }
 
 int main()
