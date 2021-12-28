5a54bcd3b7c258968987b168c3d125a48e111dc8
Shivam Dureja <shivamdureja13@gmail.com>
2021-12-28 19:53:27 +0530

    Create stringrotation.cpp

---
+++ Problems/stringrotation.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+bool isRotation(string s1, string s2)
+{
+
+    string temp = s1 + s1;
+    return (temp.find(s2) != string::npos);
+}
+
+int main()
+{
+    string str1 = "shivam", str2 = "a";
+    if (isRotation(str1, str2))
+        printf("Strings are rotations of each other");
+    else
+        printf("Strings are not rotations of each other");
+    return 0;
+}
