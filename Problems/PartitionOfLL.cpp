ad6310aafe7fb2ddc1a33e71020ae0e3f31b15a8
Shivam Dureja <shivamdureja13@gmail.com>
2021-12-31 12:46:34 +0530

    Update PartitionOfLL.cpp

--- Problems/PartitionOfLL.cpp
+++ Problems/PartitionOfLL.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+#include <iostream> /* code to partition a linked list around a value x such that all nodes less than
+                        x come before all nodes greater than or equal to x.*/
 using namespace std;
 
 struct Node
