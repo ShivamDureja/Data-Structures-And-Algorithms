f17139bd4a7c37453fcae808f0589a65b4e11757
Shivam Dureja <shivamdureja13@gmail.com>
2022-04-30 12:42:54 +0530

    Update RodCutting.cpp

--- Problems/DP/RodCutting.cpp
+++ Problems/DP/RodCutting.cpp
@@ -1,57 +1,26 @@
 #include <iostream>
-#include<algorithm> // To use the built in max function
 using namespace std;
 
-int main() {
-  int price[] = {2,5,7,8};
-  int n = 5;
-
-  // Declaring a 2D array, T
-  int T[n-1][n+1];
-
-  // Initializing the array to all zeros
-  for(int i=0; i < n-1; i++)
-  {
-    for(int j=0; j < n+1; j++ )
-    {
-      T[i][j] = 0;
-    }
-  }
-
-  for(int i=0; i < n-1; i++)
-  {
-    for(int j=0; j < n+1; j++ )
-    {
-      // First column => 0 length of rod => 0 profit
-      if(j == 0) {
-        continue;
-      }
-
-      // First row => T[i-1][j] doesn't exist so just pick the second value
-      else if(i == 0) {
-        T[i][j] = price[i] + T[i][j-i-1];
-      }
-
-      // where j <= i => T[i][j-i-1] doesn't exist so just pick the first value
-      else if(j-i-1 < 0) {
-        T[i][j] = T[i-1][j];
-      }
-
-      // using the whole expression
-      else {
-        T[i][j] = std::max(T[i-1][j], (price[i] + T[i][j-i-1]));
-      }
-    }
-  }
-  // Answer in the extreme bottom right cell
-  cout << "Maximum profit is " << T[n-2][n] << endl;
-
-
-
-  return 0;
+int max(int a, int b) {
+   return (a > b)? a : b;
 }
 
+int rodCutting(int price[], int n) {    //from price and length of n, find max profit
+   int profit[n+1];
+   profit[0] = 0;
+   int maxProfit;
 
+   for (int i = 1; i<=n; i++) {
+      maxProfit = INT_MIN;    //initially set as -ve infinity
+      for (int j = 0; j < i; j++)
+         maxProfit = max(maxProfit, price[j] + profit[i-j-1]);
+      profit[i] = maxProfit;
+   }
+   return maxProfit;
+}
 
-
-
+int main() {
+   int priceList[] = {1, 5, 8, 9, 10, 17, 17, 20};
+   int rodLength = 8;
+   cout << "Maximum Price: "<< rodCutting(priceList, rodLength);
+}
