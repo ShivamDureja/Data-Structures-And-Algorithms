879fc691c0404848f8b4f8f1f13b52b91d822a7f
Shivam Dureja <shivamdureja13@gmail.com>
2022-05-25 21:33:15 +0530

    Update LIS.cpp

--- Problems/DP/LIS.cpp
+++ Problems/DP/LIS.cpp
@@ -25,10 +25,6 @@ int LIS(int arr[],int n){
 }
 
 
-
-
-
-
 int main(){
     int arr[] = {1,7,8,4,5,6,-1,9};
     int n = 8;
@@ -36,3 +32,28 @@ int main(){
     cout<<rslt;
     return 0;
 }
+
+
+
+
+//Second Solution
+// int lis(int i,vector<int> &a){
+//     if(dp[i]!=-1) return dp[i];
+//     int ans = 1;
+//     for(int j=0;j<i;j++){
+//         if(a[i]>a[j]){
+//             ans = max(ans,lis(j,a)+1);
+//         }
+//     }
+//     return dp[i] = ans;
+// }
+
+
+// int lengthOfLis(vector<int> &nums){
+//     memset(dp,-1,sizeof(dp));
+//     int ans = 0;
+//     for(int i=0;i<nums.size();i++){
+//         ans = max(ans,lis(i,nums));
+//     }
+//     return ans;
+// }
